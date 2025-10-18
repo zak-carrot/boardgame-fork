db2a4df34f72e19a9cc1165674616e7a2f748597
jayesh <jayeshkarad1995@gmail.com>
2025-10-18 14:18:22 +0000

    terraform_files

--- terraform/output.tf
+++ terraform/output.tf
@@ -9,5 +9,3 @@ output "cluster_security_group_id" {
 output "vpc_id" {
   value = module.vpc.vpc_id
 }
-output "public_ip" { value = aws_instance.app.public_ip }
-output "app_url"   { value = "http://${aws_instance.app.public_ip}" }
